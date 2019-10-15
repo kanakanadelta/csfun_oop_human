34edb647144cc64a7f6d6addc5d9043661e387cb
Rafael Tongson <rtongson@peppymiint.local>
2019-10-15 11:06:45 -0700

    make human and class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,36 @@
 
 namespace Human
 {
+    class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+
+        public int healthProp
+        {
+            get
+            {
+                return health;
+            }
+        }
+
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Human has arrived!");
         }
     }
 }
