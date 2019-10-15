10dee71af018cd27db93fe281d201d199efb0784
Rafael Tongson <rtongson@peppymiint.local>
2019-10-15 14:15:41 -0700

    make method of attack, test and debug

--- Program.cs
+++ Program.cs
@@ -10,11 +10,15 @@ namespace Human
         public int Dexterity;
         private int health;
 
-        public int healthProp
+        public int Health
         {
             get
             {
                 return health;
+            }
+            set
+            {
+                health = value;
             }
         }
 
@@ -26,12 +30,32 @@ namespace Human
             Dexterity = 3;
             health = 100;
         }
+
+        public Human(string name, int strength, int intelligence, int dexterity, int hp)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            health = hp;
+        }
+
+        public int Attack(Human target)
+        {
+            target.Health = target.Health - Strength;
+            System.Console.WriteLine($"{Name} attacks {target.Name}");
+            return target.Health;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Human has arrived!");
+            Human guy = new Human("guy", 5, 5, 5, 100);
+            Human dude = new Human("dude");
+            guy.Attack(dude);
+            System.Console.WriteLine($"{dude.Name} has {dude.Health}hp");
         }
     }
 }
